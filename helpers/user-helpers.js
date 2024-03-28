4a274e4acaa631367282d5c1744de324f1fb6157
sharoon kp <sharoonkp267@gmail.com>
2024-03-28 20:13:20 +0530

    all things completed

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,5 +1,5 @@
-var db = require ('../config/connection')
-var collName = require("../config/collections") 
+var db = require('../config/connection')
+var collName = require("../config/collections")
 const bcrypt = require('bcrypt')
 const promise = require('promise')
 const { reject, resolve } = require('promise')
@@ -11,196 +11,196 @@ var nodemailer = require('nodemailer');
 
 
 module.exports = {
-    doSignup:(userData)=>{
-        return new promise(async(resolve,reject)=>{
-            
-            let respo ={}
-            userData.password =await bcrypt.hash(userData.password,10)
-            db.get().collection(collName.user_collection).findOne({$or:[{email:userData.email},{name:userData.name}]}).then((status)=>{
-                if(status){
-                    respo.status=true
+    doSignup: (userData) => {
+        return new promise(async (resolve, reject) => {
+
+            let respo = {}
+            userData.password = await bcrypt.hash(userData.password, 10)
+            db.get().collection(collName.user_collection).findOne({ $or: [{ email: userData.email }, { name: userData.name }] }).then((status) => {
+                if (status) {
+                    respo.status = true
                     resolve(respo)
-                }else{
+                } else {
                     db.get().collection(collName.user_collection).insertOne(userData)
-                    resolve({status:false})
+                    resolve({ status: false })
                 }
             })
-                
-            
-            })
-            
-        
+
+
+        })
+
+
     },
 
 
-    doLogin:(loginData)=>{
-        return new promise(async(resolve,reject)=>{
+    doLogin: (loginData) => {
+        return new promise(async (resolve, reject) => {
             let Status = false
-            let response= {}
-            let user = await db.get().collection(collName.user_collection).findOne({email:loginData.email})
-            if(user){
-               
-                bcrypt.compare(loginData.password,user.password).then((status)=>{
-                    
-                    if(status){
+            let response = {}
+            let user = await db.get().collection(collName.user_collection).findOne({ email: loginData.email })
+            if (user) {
+
+                bcrypt.compare(loginData.password, user.password).then((status) => {
+
+                    if (status) {
                         response.user = user
-                        response.status =true
+                        response.status = true
                         resolve(response)
-                        
-                    }else{
-                        resolve({status:false})
+
+                    } else {
+                        resolve({ status: false })
                     }
 
-                })  
-            }else{
+                })
+            } else {
                 console.log("login failed");
-                resolve({status:false})
+                resolve({ status: false })
             }
         })
     },
 
-    getHospital:(departments,district)=>{
+    getHospital: (departments, district) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.hospital_collection).find({departments:departments,district:district}).toArray().then((response)=>{
+            db.get().collection(collName.hospital_collection).find({ departments: departments, district: district }).toArray().then((response) => {
                 resolve(response)
             })
         });
     },
-    getDoctors:(departments,id)=>{
-        console.log(departments,id);
+    getDoctors: (departments, id) => {
+        console.log(departments, id);
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.doctor_collection).find({Specialization:departments,hospitalCode:id}).toArray().then((response)=>{
+            db.get().collection(collName.doctor_collection).find({ Specialization: departments, hospitalCode: id }).toArray().then((response) => {
                 resolve(response)
             })
         });
     },
-    hospitalLogin:(id)=>{
-        respo={}
-        return new Promise(async(resolve, reject) => {
-          let status = await db.get().collection(collName.hospital_collection).findOne({loginCode:id})
-          if(status){
-              respo.details=status
-              respo.status = true
-              resolve(respo)
-          }else{
-              resolve({status:false})
-          }
+    hospitalLogin: (id) => {
+        respo = {}
+        return new Promise(async (resolve, reject) => {
+            let status = await db.get().collection(collName.hospital_collection).findOne({ loginCode: id })
+            if (status) {
+                respo.details = status
+                respo.status = true
+                resolve(respo)
+            } else {
+                resolve({ status: false })
+            }
         });
     },
 
-    addAppoinment:(details)=>{
+    addAppoinment: (details) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).insertOne(details).then((response)=>{
+            db.get().collection(collName.appoinment_collection).insertOne(details).then((response) => {
                 resolve(response)
             })
         });
     },
 
-    getDoctorName:(id)=>{
+    getDoctorName: (id) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.doctor_collection).findOne({_id:ObjectId(id)}).then((response)=>{
+            db.get().collection(collName.doctor_collection).findOne({ _id: ObjectId(id) }).then((response) => {
                 resolve(response.name)
             })
         });
     },
-    
-    getAppoinmentRequests:(hospitalId)=>{
-        console.log("where",hospitalId);
+
+    getAppoinmentRequests: (hospitalId) => {
+        console.log("where", hospitalId);
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).find({hospitalId:hospitalId,status:"waiting"}).toArray().then((response)=>{
+            db.get().collection(collName.appoinment_collection).find({ hospitalId: hospitalId, status: "waiting" }).toArray().then((response) => {
                 console.log(response);
                 resolve(response)
             })
         });
     },
 
-    sentApproveMail:(email,id)=>{
+    sentApproveMail: (email, id, time) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).updateOne({_id:ObjectId(id)},{$set:{status:"booked"}}).then((response)=>{
+            db.get().collection(collName.appoinment_collection).updateOne({ _id: ObjectId(id) }, { $set: { status: "booked" } }).then((response) => {
                 var transporter = nodemailer.createTransport({
                     secure: true,
                     port: 465,
-                  service: 'gmail',
-                  auth: {
-                    user: "ecommerce1419@gmail.com",
-                    pass: "iqtyaldszzgoweap"
-                  }
+                    service: 'gmail',
+                    auth: {
+                        user: "ecommerce1419@gmail.com",
+                        pass: "iqtyaldszzgoweap"
+                    }
                 });
-                
+
                 var mailOptions = {
-                  from: 'ecommerce1419@gmail.com',
-                  to: email,
-                  subject: 'Appoinment Update',
-                  text: 'Your Appoinment has been Booked'
+                    from: 'ecommerce1419@gmail.com',
+                    to: email,
+                    subject: 'Appoinment Update',
+                    text: `Your Appoinment has been Booked on ${time}`
                 };
-                
-                transporter.sendMail(mailOptions, function(error, info){
-                  if (error) {
-                    console.log(error);
-                  } else {
-                    console.log('Email sent: ' + info.response);
-                  }
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
                 });
                 resolve(response)
             })
         });
     },
-    sentRejectMail:(email,id)=>{
+    sentRejectMail: (email, id, rejectReason) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).deleteOne({_id:ObjectId(id)}).then((response)=>{
+            db.get().collection(collName.appoinment_collection).deleteOne({ _id: ObjectId(id) }).then((response) => {
                 var transporter = nodemailer.createTransport({
                     secure: true,
                     port: 465,
-                  service: 'gmail',
-                  auth: {
-                    user: "ecommerce1419@gmail.com",
-                    pass: "iqtyaldszzgoweap"
-                  }
+                    service: 'gmail',
+                    auth: {
+                        user: "ecommerce1419@gmail.com",
+                        pass: "iqtyaldszzgoweap"
+                    }
                 });
-                
+
                 var mailOptions = {
-                  from: 'ecommerce1419@gmail.com',
-                  to: email,
-                  subject: 'Appoinment Update',
-                  text: 'Your Appoinment has been rejected'
+                    from: 'ecommerce1419@gmail.com',
+                    to: email,
+                    subject: 'Appoinment Update',
+                    text: `Your Appoinment has been rejected due to the reason ${rejectReason} `
                 };
-                
-                transporter.sendMail(mailOptions, function(error, info){
-                  if (error) {
-                    console.log(error);
-                  } else {
-                    console.log('Email sent: ' + info.response);
-                  }
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
                 });
                 resolve(response)
             })
         });
     },
 
-    getAcceptedAppoinment:(hospitalId)=>{
-        console.log("where",hospitalId);
+    getAcceptedAppoinment: (hospitalId) => {
+        console.log("where", hospitalId);
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).find({hospitalId:hospitalId,status:"booked"}).toArray().then((response)=>{
+            db.get().collection(collName.appoinment_collection).find({ hospitalId: hospitalId, status: "booked" }).toArray().then((response) => {
                 console.log(response);
                 resolve(response)
             })
         });
     },
 
-    deleteAppoinment:(id)=>{
+    deleteAppoinment: (id) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.appoinment_collection).deleteOne({_id:ObjectId(id)}).then((response)=>{
+            db.get().collection(collName.appoinment_collection).deleteOne({ _id: ObjectId(id) }).then((response) => {
                 resolve(response)
             })
         })
     },
 
-    getAllDoctors:(hospitalId)=>{
+    getAllDoctors: (hospitalId) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collName.doctor_collection).find({hospitalCode:hospitalId}).toArray().then((response)=>{
+            db.get().collection(collName.doctor_collection).find({ hospitalCode: hospitalId }).toArray().then((response) => {
                 resolve(response)
             })
         });
     }
-    
+
 }
